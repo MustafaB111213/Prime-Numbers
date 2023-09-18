c885be1e291a321e27f29dc3a36f5a6f7af989cc
~YBN~ <~YBN~@DESKTOP-G4BGE5J>
2023-09-18 21:03:40 +0300

    Prime Numbers

--- Prime Numbers/src/PrimeCounter.java
+++ Prime Numbers/src/PrimeCounter.java
@@ -2,8 +2,46 @@
 public class PrimeCounter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		PrimeCounter primeCounter = new PrimeCounter();
+		
+
+		System.out.println(primeCounter.isPrime(13));
+		System.out.println(primeCounter.primeNumberFromOneTo(30));
+		
+	}
+	boolean isPrime(int n) {
+		boolean isPrime = false;
+		if (n>0) {
+			if (n!=2) {
+				for (int i = 2; i < Math.sqrt(n)+1 ; i++) {
+					if (n%i==0) {
+						isPrime = false;
+						break;
+					}
+					else {
+						isPrime = true;
+					}
+				}
+			}
+			else {
+				isPrime=true;
+			}
+		}
+		return isPrime;
+	}
+	
+	int primeNumberFromOneTo(int n) {
+		int primeCounter = 0;
+		
+		for (int i = 0; i < n; i++) {
+			if (isPrime(i)) {
+				primeCounter++;
+			}
+				
+		}
+		
+		return primeCounter;
 	}
 
 }
